ad3cdbca5e70e1106f1e486b881b5abf76a087b4
Teis <teisgossen@gmail.com>
2018-02-16 09:46:49 +0100

    Added Animations
    Added a player character with animations and a script to handle the animations

---
+++ Animation/CharacterAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    const float locomationAnimationSmoothTime = .1f;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomationAnimationSmoothTime, Time.deltaTime);
+	}
+}
